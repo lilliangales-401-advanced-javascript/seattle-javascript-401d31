c7956571e47d812a64c83fb67040377095d4e0d0
Lillian Gales <lilliangales@ymail.com>
2019-08-12 15:23:37 -0700

    checking commit

--- curriculum/class-01/lab/starter-code/index.js
+++ curriculum/class-01/lab/starter-code/index.js
@@ -7,5 +7,7 @@ const validator = require('./lib/validator');
 
 console.log('I am going to test if a value is one');
 console.log(validator.isValid(123123, 'one'));
+console.log(validator.isValid(1, 'one'));
 
-console.log(validator.isValid('I am a negative value: -1', 'negative'));
+console.log(validator.isValid(12, 'one'));
+
